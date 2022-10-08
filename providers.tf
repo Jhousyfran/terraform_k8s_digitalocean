bad3dd89a2d8d1b6db8bed2599c81df009c8fdce
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2022-10-08 13:27:23 -0300

    first commit

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.22.3"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+}
