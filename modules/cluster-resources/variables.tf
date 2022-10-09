ccfaaec83639758415ed2ca166a5e75b35b23e2c
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2022-10-09 16:44:34 -0300

    feat: adiciona variaveis para kubernetes provider

--- modules/cluster-resources/variables.tf
+++ modules/cluster-resources/variables.tf
@@ -2,8 +2,12 @@ variable "cluster_name" {}
 
 variable "region" {}
 
-variable "letsencrypt_email" {}
-
 variable "domains" {}
 
 variable "loadbalancer_id" {}
+
+variable "cluster_endpoint" {}
+
+variable "cluster_token" {}
+
+variable "cluster_ca_certificate" {}
