4086c5aea01f0928928f4c0f7f676722c0cfac82
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2022-10-09 16:41:03 -0300

    feat: adiciona modulo dns

---
+++ modules/dns/main.tf
@@ -0,0 +1,29 @@
+resource "digitalocean_domain" "domains" {
+    for_each = toset(var.domains)
+    name = each.value
+}
+
+resource "digitalocean_record" "a_records" {
+  for_each = toset(var.domains)
+  domain = each.value
+  type   = "A"
+  ttl = 60
+  name   = "@"
+  value  = var.loadbalancer_id
+  depends_on = [
+    digitalocean_domain.domains,
+    var.ingress_id
+  ]
+}
+
+resource "digitalocean_record" "cname_redirects" {
+  for_each = toset(var.domains)
+  domain = each.value
+  type   = "CNAME"
+  ttl = 60
+  name   = "www"
+  value  = "@"
+  depends_on = [
+    digitalocean_domain.domains
+  ]
+}
---
+++ modules/dns/variables.tf
@@ -0,0 +1,5 @@
+variable "domains" {}
+
+variable "loadbalancer_id" {}
+
+variable "ingress_id" {}
